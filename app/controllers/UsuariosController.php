44cb3a152d359af31aad635fe6601ab4b0540534
alan-livinsky <alan.livinsky@gmail.com>
2021-06-13 12:35:17 -0300

    Header Content Type Json

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -26,9 +26,7 @@ class UsuariosController{
     public static function retornarUsuario($request,$response,$args){
         $usuario=Usuarios::buscar_usuario($args['usuario'],$args['contrasea']);
         $response->getBody()->write(json_encode($usuario));
-        return $response;
-
-        /*->withHeader('Content-type','application/json')*/
+        return $response->withHeader('Content-type','application/json');
     }
 
 }
