59e6fa8e358544f36cbfeecdd5fe7aecb1f47c29
alan-livinsky <alan.livinsky@gmail.com>
2021-06-12 23:16:22 -0300

    ewqwe

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -25,7 +25,7 @@ class UsuariosController{
 
     public static function retornarUsuario($request,$response,$args){
         $usuario=Usuarios::buscar_usuario($args['usuario'],$args['contrasea']);
-        $response=json_encode($usuario);
+        $response->getBody()->write(json_encode($usuario));
         return $response;
 
         /*->withHeader('Content-type','application/json')*/
