789df22d5de2cdd441efd5129c9ba2fc5135c2e1
alan-livinsky <alan.livinsky@gmail.com>
2021-06-19 20:28:43 -0300

    asas

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -22,8 +22,8 @@ class UsuariosController{
         return $response;*/
 
         $usuario=new Usuarios();
-        $estadoRegistro=$usuario->registrar_usuario($json[0]['id_usuario'],$json[0]['email'],$json[0]['contraseña']
-                                                    ,$json[0]['nombre'],$json[0]['apellido'],$json[0]['tipo_usuario']);
+        $estadoRegistro=$usuario->registrar_usuario($data[0]['id_usuario'],$data[0]['email'],$data[0]['contraseña']
+                                                    ,$data[0]['nombre'],$data[0]['apellido'],$data[0]['tipo_usuario']);
         $response->getBody()->write($estadoRegistro);                                    
         return $response;
     }
