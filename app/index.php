418f27dde277a9be1aed74ca3f81849fb18c9bd9
alan-livinsky <alan.livinsky@gmail.com>
2021-06-22 19:07:55 -0300

    middleware

--- app/index.php
+++ app/index.php
@@ -29,33 +29,29 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 //CORS
-$app->add(function (Request $request, RequestHandlerInterface $handler): Response {
-    
+$app->add(function (Request $request, RequestHandlerInterface $handler): Response {  
     $response = $handler->handle($request);
     $requestHeaders = $request->getHeaderLine('Access-Control-Request-Headers');
     $response = $response->withHeader('Access-Control-Allow-Origin', '*');
     $response = $response->withHeader('Access-Control-Allow-Methods', 'get,post');
     $response = $response->withHeader('Access-Control-Allow-Headers', $requestHeaders);
-
     return $response;
 });
 
 
+$app->get('[/]',function(Request $request, Response $response, array $args) { 
+    $response->getBody()->write("Bienvenido a SAE-SH");
+    return $response;
+});
+
 $app->group('/Usuarios', function (RouteCollectorProxy $group) {
-
-    $group->get('/loguin',function(Request $request, Response $response, array $args) { 
-        $response->getBody()->write("Bienvenido a SAE-SH");
-        return $response;
-    });
    //La ñ no funciona
-    $group->get('/loguin/{usuario}/{contrasea}',\UsuariosController::class.':retornarUsuario');
-
+    $group->get('/loguin/{usuario}/{contrasea}',
+            \UsuariosController::class.':retornarUsuario')->add(new JWT_Middleware());
     $group->post('/registro',\UsuariosController::class.':retornarEstadoRegistro');
-   
 });
 
 $app->group('/Token', function (RouteCollectorProxy $group) {
-
     $group->get('/loguin',function(Request $request, Response $response, array $args) { 
         
         /*
@@ -79,11 +75,21 @@ $app->group('/Token', function (RouteCollectorProxy $group) {
 
         echo $JWT;*/
         
-    
-        
         $privateKey = <<<EOD
         -----BEGIN RSA PRIVATE KEY-----
-        clave-de-32-caracteres-super-ultra-secreta-para-generar-JWT
+        MIICXAIBAAKBgQC8kGa1pSjbSYZVebtTRBLxBz5H4i2p/llLCrEeQhta5kaQu/Rn
+        vuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t0tyazyZ8JXw+KgXTxldMPEL9
+        5+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4ehde/zUxo6UvS7UrBQIDAQAB
+        AoGAb/MXV46XxCFRxNuB8LyAtmLDgi/xRnTAlMHjSACddwkyKem8//8eZtw9fzxz
+        bWZ/1/doQOuHBGYZU8aDzzj59FZ78dyzNFoF91hbvZKkg+6wGyd/LrGVEB+Xre0J
+        Nil0GReM2AHDNZUYRv+HYJPIOrB0CRczLQsgFJ8K6aAD6F0CQQDzbpjYdx10qgK1
+        cP59UHiHjPZYC0loEsk7s+hUmT3QHerAQJMZWC11Qrn2N+ybwwNblDKv+s5qgMQ5
+        5tNoQ9IfAkEAxkyffU6ythpg/H0Ixe1I2rd0GbF05biIzO/i77Det3n4YsJVlDck
+        ZkcvY3SK2iRIL4c9yY6hlIhs+K9wXTtGWwJBAO9Dskl48mO7woPR9uD22jDpNSwe
+        k90OMepTjzSvlhjbfuPN1IdhqvSJTDychRwn1kIJ7LQZgQ8fVz9OCFZ/6qMCQGOb
+        qaGwHmUK6xzpUbbacnYrIM6nLSkXgOAwv7XXCojvY614ILTK3iXiLBOxPu5Eu13k
+        eUz9sHyD6vkgZzjtxXECQAkp4Xerf5TGfQXGXhxIX52yH+N2LtujCdkQZjXAsGdm
+        B2zNzvrlgRmgBrklMTrMYgm1NPcW+bRLGcwgW2PTvNM=
         -----END RSA PRIVATE KEY-----
         EOD;
 
@@ -115,19 +121,11 @@ $app->group('/Token', function (RouteCollectorProxy $group) {
         $decoded_array = (array) $decoded;
         echo "Decode:\n" . print_r($decoded_array, true) . "\n";
 
+        $response=$jwt;
+        return $response;
     });
-  
 });
 
-
-
-
-
-
-$app->get('[/]',function(Request $request, Response $response, array $args) { 
-    $response->getBody()->write("Hello");
-    return $response;
-});
 
 
 
