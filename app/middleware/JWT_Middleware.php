b5fc374e4bd90168142c018088d403097fa7ac17
alan-livinsky <alan.livinsky@gmail.com>
2021-06-22 19:28:20 -0300

    ASAS

--- app/middleware/JWT_Middleware.php
+++ app/middleware/JWT_Middleware.php
@@ -1,14 +1,30 @@
 <?php
 
-class JWT_Middleware
+
+class ExampleBeforeMiddleware
 {
-    public function __invoke($request, $response, $next){
+   
+    public function __invoke(Request $request, RequestHandler $handler): Response
+    {
+        
+    }
+}
 
-        $response = $next($request, $response);
 
-        echo $response;
-        echo "test";
 
+class JWT_Middleware{
+
+    public function __invoke(Request $request, RequestHandler $handler): Response{
+
+        $response = $handler->handle($request);
+        $existingContent = (string) $response->getBody();
+    
+        $response = new Response();
+        $response->getBody()->write('BEFORE' . $existingContent);
+    
+        return $response;
+
+    
         /*
         $privateKey = <<<EOD
         -----BEGIN RSA PRIVATE KEY-----
