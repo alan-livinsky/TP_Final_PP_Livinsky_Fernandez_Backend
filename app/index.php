0878fe43dd9bc0197866c1839979f2c2b2508662
alan-livinsky <alan.livinsky@gmail.com>
2021-06-22 19:09:43 -0300

    ssd

--- app/index.php
+++ app/index.php
@@ -12,7 +12,8 @@ use Slim\Routing\RouteCollectorProxy;
 use Slim\Routing\RouteContext;
 
 require __DIR__ .'/../vendor/autoload.php';
-require __DIR__ .'/acceso_a_datos/Acceso_a_datos.php';
+require __DIR__ .'/middleware/Acceso_a_datos.php';
+require __DIR__ .'/acceso_a_datos/JWT_Middleware.php';
 require __DIR__ . '/controllers/UsuariosController.php';
 require __DIR__ . '/entidades/Usuarios.php';
 require __DIR__ . '/entidades/Cursos.php';
