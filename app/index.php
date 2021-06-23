946b9c015c22cc2ec23c479c257e6ee03aa126bd
alan-livinsky <alan.livinsky@gmail.com>
2021-06-23 19:33:00 -0300

    dfsd

--- app/index.php
+++ app/index.php
@@ -43,7 +43,7 @@ $app->add(function (Request $request, RequestHandlerInterface $handler): Respons
 $app->add(new Tuupola\Middleware\JwtAuthentication([
     "secure" => false,//Evitar error https
     "secret" => $_ENV['JWT_SECRET'],
-    "ignore" => ["/Usuarios/registro", "/Usuarios/loguin"],
+    "ignore" => ["/","/Usuarios/registro", "/Usuarios/loguin"],
     
     "error" => function ($response, $arguments){
         $data["status"]="error";
