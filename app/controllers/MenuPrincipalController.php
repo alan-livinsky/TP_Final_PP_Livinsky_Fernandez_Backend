44dedb06bd455af2d19b98ab7ae0d4de27da8fe5
alan-livinsky <alan.livinsky@gmail.com>
2021-06-23 22:34:11 -0300

    dssd

--- app/controllers/MenuPrincipalController.php
+++ app/controllers/MenuPrincipalController.php
@@ -3,7 +3,7 @@
 class MenuPrincipalController{
 
   public static function retornarAccesoMenuPrincipal($request,$response,$args){
-      return $response->withAddedHeader('Location','https://tp-final-pp-liv-ferz-frontend.herokuapp.com/')
+      return $response->withAddedHeader('Location','https://tp-final-pp-liv-ferz-frontend.herokuapp.com/Menu_Principal.php')
                         ->withStatus(302);
   }
 
