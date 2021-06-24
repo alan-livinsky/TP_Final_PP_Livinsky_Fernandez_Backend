8f23ca959c4f7c9711224a76acbe3d6e8bc020d5
alan-livinsky <alan.livinsky@gmail.com>
2021-06-23 22:47:53 -0300

    dsdsd

--- app/controllers/MenuPrincipalController.php
+++ app/controllers/MenuPrincipalController.php
@@ -3,8 +3,8 @@
 class MenuPrincipalController{
 
   public static function retornarAccesoMenuPrincipal($request,$response,$args){
-      return $response->withAddedHeader('Location','https://tp-final-pp-liv-ferz-frontend.herokuapp.com/Menu_Principal.php')
-                        ->withStatus(302);
+      $response=MenuPrincipal::getURL();
+      return $response;
   }
 
 
--- app/entidades/MenuPrincipal.php
+++ app/entidades/MenuPrincipal.php
@@ -3,6 +3,16 @@
     class MenuPrincipal{
         public $opcionesGenerales;//Tienen que ser arrays
         public $opcionesEspecificas;
+        public static $url="https://tp-final-pp-liv-ferz-frontend.herokuapp.com/Menu_Principal.php";
+
+
+
+        public static function getURL(){
+            return $this->url;
+        }
+
+
+
     }
 
 ?>
