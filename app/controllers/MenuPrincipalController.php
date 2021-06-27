1a2f597a99b1aaef5e08fe98119fc8ba92eba014
alan-livinsky <alan.livinsky@gmail.com>
2021-06-26 23:00:24 -0300

    git push

--- app/controllers/MenuPrincipalController.php
+++ app/controllers/MenuPrincipalController.php
@@ -3,7 +3,7 @@
 class MenuPrincipalController{
 
   public static function retornarAccesoMenuPrincipal($request,$response,$args){
-      return $response-withStatus(200); 
+      return $response->withStatus(200); 
   }
 
 }
