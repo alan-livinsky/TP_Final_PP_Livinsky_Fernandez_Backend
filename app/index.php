a02f303d2444b856d0984ce500b0e2b865a83cba
alan-livinsky <alan.livinsky@gmail.com>
2021-06-29 21:55:55 -0300

    as

--- app/index.php
+++ app/index.php
@@ -16,6 +16,8 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
+use Firebase\JWT\JWT;
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/acceso_a_datos/Acceso_a_datos.php';
 require __DIR__ . '/controllers/UsuariosController.php';
@@ -26,7 +28,7 @@ require __DIR__ . '/entidades/Ejercicios.php';
 require __DIR__ . '/entidades/Cursos.php';
 require __DIR__ . '/entidades/MenuPrincipal.php';
 
-use Firebase\JWT\JWT;
+
 
 use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable('../');
