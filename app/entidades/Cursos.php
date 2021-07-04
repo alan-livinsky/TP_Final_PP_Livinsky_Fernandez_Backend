3ca97d182611c4845cca96cc83bea02f6b9f2d29
alan-livinsky <alan.livinsky@gmail.com>
2021-07-04 18:18:56 -0300

    ds

--- app/entidades/Cursos.php
+++ app/entidades/Cursos.php
@@ -16,12 +16,14 @@
 
         public function asociarUsuarioCurso($año,$comision,$turno,$id_usuario){
             $cursoEncontrado=$this->buscarCurso($año,$comision,$turno);
+            $id_curso=$cursoEncontrado[0]->id_curso;
             var_dump($cursoEncontrado);
+
              if(count($cursoEncontrado)!=0){
                 $accesoDatos=Acceso_a_datos::obtenerConexionBD();
 
                 $consulta=$accesoDatos->prepararConsulta("INSERT INTO usuarios_por_curso
-                                                            VALUES ($id_usuario,$cursoEncontrado[0]->id_curso)");
+                                                            VALUES ($id_usuario,$id_curso)");
                 $consulta->execute(); 
              }
         }
