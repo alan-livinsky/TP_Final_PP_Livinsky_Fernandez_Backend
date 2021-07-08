2e00b0f7f6ae8e8030fdaed4a5edb4330b1483f2
alan-livinsky <alan.livinsky@gmail.com>
2021-07-08 01:49:08 -0300

    sd

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -113,8 +113,8 @@ class UsuariosController{
         $mail->isSMTP();                                      //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';               //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                             //Enable SMTP authentication
-        $mail->Username="SAESHitbeltran@gmail.com";               //SMTP username
-        $mail->Password="SAESHlivfer";                        //SMTP password
+        $mail->Username='SAESHitbeltran@gmail.com';               //SMTP username
+        $mail->Password='SAESHlivfer';                        //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;      //Enable implicit TLS encryption
         $mail->Port       = 465;     
 
