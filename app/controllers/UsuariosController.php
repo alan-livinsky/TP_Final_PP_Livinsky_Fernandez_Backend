ead795c19c3e9237de669d62214cccdbc51b58e3
alan-livinsky <alan.livinsky@gmail.com>
2021-07-18 18:23:30 -0300

    assdds

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -103,90 +103,31 @@ class UsuariosController{
 
     //Use the second parameter of json_decode to make it return an array:
     //$result = json_decode($data, true);
-
-
     //function sendVerificationEmail($email,$id)
-    //Parametro de contenido???
-    public function enviarEmailDeRecuperacion($email)
-    {      
+    public function enviarEmailDeRecuperacion($email){      
         $mail=new PHPMailer;
-        $mail->SMTPDebug=SMTP::DEBUG_SERVER;               //Enable verbose debug output
-        $mail->isSMTP();                                   //Send using SMTP
-        $mail->Host='smtp.gmail.com';                      //Set the SMTP server to send through
-        $mail->SMTPAuth=true;                              //Enable SMTP authentication
-        $mail->Username='SAESHitbeltran@gmail.com';        //SMTP username
-        $mail->Password='rwbiofucouofrvth';                //SMTP contraseña de aplicacion (autentificacion en 2 pasos)
-        $mail->SMTPSecure=PHPMailer::ENCRYPTION_SMTPS;     //Enable implicit TLS encryption
+        $mail->SMTPDebug=SMTP::DEBUG_SERVER;                  //Enable verbose debug output
+        $mail->isSMTP();                                      //Send using SMTP
+        $mail->Host='smtp.gmail.com';                         //Set the SMTP server to send through
+        $mail->SMTPAuth=true;                                 //Enable SMTP authentication
+        $mail->Username='SAESHitbeltran@gmail.com';           //SMTP username
+        $mail->Password='rwbiofucouofrvth';                   //SMTP contraseña de aplicacion (autentificacion en 2 pasos)
+        $mail->SMTPSecure=PHPMailer::ENCRYPTION_SMTPS;        //Enable implicit TLS encryption
         $mail->Port=465;     
 
         //Recipients
-        $mail->setFrom('SAESHitbeltran@gmail.com','SAE-SH'); //Add a recipient 
-        $mail->addAddress($email,'Usuario');                 //Name is optional
+        $mail->setFrom('SAESHitbeltran@gmail.com','SAE-SH');  //Add a recipient 
+        $mail->addAddress($email,'Usuario');                  //Name is optional
 
         //Attachments
-        //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-        //$mail->addAttachment('/tmp/image.jpg','new.jpg');    / /Optional name
+        //$mail->addAttachment('/var/tmp/file.tar.gz');       //Add attachments
+        //$mail->addAttachment('/tmp/image.jpg','new.jpg');   //Optional name
 
         //Content
         $mail->Subject = 'Here is the subject';
-        $mail->Body='
-        <body style="margin:0;padding:0;">
-	<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
-		<tr>
-			<td align="center" style="padding:0;">
-				<table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
-
-					<tr>
-						<td align="center" style="padding:40px 0 30px 0;background:#f0b13c;">
-							<img src="https://j7n9h4d9.rocketcdn.me/wp-content/uploads/2020/05/yancy-min-EzSyFRfNP_c-unsplash-768x512.jpg" alt="" width="300" style="height:auto;display:block;" />
-						</td>
-					</tr>
-
-					<tr>
-						<td style="padding:36px 30px 42px 30px;">
-							<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-								<tr>
-									<td style="padding:0 0 36px 0;color:#153643;">
-										<h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;">SAE-SH</h1>
-										<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan et dictum, nisi libero ultricies ipsum, posuere neque at erat.</p>
-										<p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://www.example.com" style="color:#ee4c50;text-decoration:underline;">In tempus felis blandit</a></p>
-									</td>
-								</tr>
-								<tr>
-									<td style="padding:0;">
-										<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-											<tr>
-												<td style="width:260px;padding:0;vertical-align:top;color:#153643;">
-													<p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><img src="https://assets.codepen.io/210284/left.gif" alt="" width="260" style="height:auto;display:block;" /></p>
-													<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, est nisi libero ultricies ipsum, in posuere mauris neque at erat.</p>
-													<p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://www.example.com" style="color:#ee4c50;text-decoration:underline;">Blandit ipsum volutpat sed</a></p>
-												</td>
-												<td style="width:20px;padding:0;font-size:0;line-height:0;">&nbsp;</td>
-												<td style="width:260px;padding:0;vertical-align:top;color:#153643;">
-													<p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><img src="https://assets.codepen.io/210284/right.gif" alt="" width="260" style="height:auto;display:block;" /></p>
-													<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Morbi porttitor, eget est accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed.</p>
-													<p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://www.example.com" style="color:#ee4c50;text-decoration:underline;">In tempus felis blandit</a></p>
-												</td>
-											</tr>
-										</table>
-									</td>
-								</tr>
-							</table>
-						</td>
-					</tr>
-				
-						<td style="padding:30px;background:#f38a28;">
-							<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
-								<tr>
-									<td style="padding:0;width:50%;" align="left">
-										<p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">
-											&reg; SAE-SH
-										</p>
-									</td>
-						<td style="padding:0;width:50%;" align="right">';
+        $mail->Body=file_get_contents('../Email/Email_Recuperacion_Contraseña.php');
      
-        
-        $mail->isHTML(true);//Set email format to HTML
+        $mail->isHTML(true); //Set email format to HTML
         $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
         if($mail->send()){
