4c304c9170d2d23aa0a90b5d79b780c1504a8ad2
alan-livinsky <alan.livinsky@gmail.com>
2021-07-21 01:19:22 -0300

    assa

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -112,6 +112,9 @@ class UsuariosController{
         $datosDelUsuario=json_decode($datosDelUsuario);
         $email=$datosDelUsuario->email;
 
+
+
+        /*
         $mail=new PHPMailer;
         $mail->SMTPDebug=SMTP::DEBUG_SERVER;                  //Enable verbose debug output
         $mail->isSMTP();                                      //Send using SMTP
@@ -135,15 +138,17 @@ class UsuariosController{
         $mail->Body=file_get_contents(getcwd().'/Email/Email_Recuperacion_Contraseña.php');
      
         $mail->isHTML(true); //Set email format to HTML
-        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';*/
 
+        /*
         if($mail->send()){
             $response->getBody()->write(Json_encode("Se ha enviado el Email.Por favor verifique su casilla de correo."));
         }
         else{
 
             $response->getBody()->write(Json_encode("Ah ocurrido un error.El email no pudo enviarse"));
-        }
+        }*/
+        $response->getBody()->write(Json_encode("Ah ocurrido un error.El email no pudo enviarse"));
         return $response->withHeader('Content-type','application/json');
 
     }
