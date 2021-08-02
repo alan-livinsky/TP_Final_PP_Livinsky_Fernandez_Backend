e684624eae55f5ceea746121562389f0905caefc
alan-livinsky <alan.livinsky@gmail.com>
2021-08-02 10:34:05 -0300

    sdsd

--- app/controllers/MaterialesController.php
+++ app/controllers/MaterialesController.php
@@ -3,13 +3,15 @@
 class MaterialesController{
 
   public static function retornarListaMateriales($request,$response,$args){
-    $listaMateriales=Materiales::buscar_lista_materiales();
+    $materiales=new Materiales();
+    $listaMateriales=$materiales->buscar_lista_materiales();
     $response->getBody()->write(json_encode($listaMateriales));
     return $response;
   }
 
   public static function retornarDatosMaterial($request,$response,$args){
-    $listaMateriales=Materiales::buscarDatosMaterial($args['material']);
+    $materiales=new Materiales();
+    $listaMateriales=$materiales->buscarDatosMaterial($args['material']);
     $response->getBody()->write(json_encode($listaMateriales));
     return $response;
   }
