a6c415fa923f917e55fb4350f37bf41605537ecf
alan-livinsky <alan.livinsky@gmail.com>
2021-08-15 06:10:00 -0300

    asas

--- app/entidades/Usuarios.php
+++ app/entidades/Usuarios.php
@@ -25,7 +25,7 @@
 
             var_dump($hash);
 
-            if (password_verify($contraseña,$hash->contraseña)){
+            if (password_verify($contraseña,$hash[0]->contraseña)){
                 $contraseña=$hash;
                 $consulta=$accesoDatos->prepararConsulta("SELECT * FROM usuarios WHERE email='$email' AND contraseña='$contraseña'");
                 $consulta->execute();
