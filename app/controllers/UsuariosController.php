23db152ecbf4285e118ecb63ab1f648c21f0a252
alan-livinsky <alan.livinsky@gmail.com>
2021-08-16 19:58:14 -0300

    asas

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -212,12 +212,12 @@ class UsuariosController{
 
            // generarTokenEmailRecuperacion($email);
 
-            /*
+            
             $token=random_bytes(32);
             date_default_timezone_set('America/Argentina/Buenos_Aires');
             $fechaHoraActual=date('Y/m/d H:i:s');
             $fechaVencimiento=date('Y/m/d H:i:s',strtotime("$fechaHoraActual +1 day"));
-           
+            /*
             
             $accesoDatos=Acceso_a_datos::obtenerConexionBD(); 
             $consulta=$accesoDatos->prepararConsulta("INSERT INTO solicitudes_recuperar_contraseña 
