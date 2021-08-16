0932ac308025301399d5f0ebac19193a453b6137
alan-livinsky <alan.livinsky@gmail.com>
2021-08-16 19:18:28 -0300

    as

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -225,7 +225,7 @@ class UsuariosController{
 
             //Content
             $mail->Subject = 'Recuperacion de acceso a cuenta';
-            $mail->Body=prepararEmailDeRecuperacion($email);
+            $mail->Body="a";
         
             $mail->isHTML(true); //Set email format to HTML
             $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
