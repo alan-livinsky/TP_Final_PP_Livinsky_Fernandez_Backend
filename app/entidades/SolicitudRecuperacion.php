02756703fe87182c4e576a48fb41b41152d4c700
alan-livinsky <alan.livinsky@gmail.com>
2021-08-16 21:56:00 -0300

    asas

--- app/entidades/SolicitudRecuperacion.php
+++ app/entidades/SolicitudRecuperacion.php
@@ -3,8 +3,8 @@
     class SolicitudRecuperacion{
 
     public static function generarTokenEmailRecuperacion($email){  
-        //$token=random_bytes(32);
-        $token="pepe";
+        $token=random_bytes(32);
+        //$token="pepe";
         date_default_timezone_set('America/Argentina/Buenos_Aires');
         $fechaHoraActual=date('Y/m/d H:i:s');
         $fechaVencimiento=date('Y/m/d H:i:s',strtotime("$fechaHoraActual +1 day"));
@@ -12,7 +12,7 @@
         $accesoDatos=Acceso_a_datos::obtenerConexionBD();
         $consulta=$accesoDatos->prepararConsulta("INSERT INTO solicitudes_recuperar_contraseña 
                                                 VALUES
-                                                (1,'$email','$token','$fechaVencimiento')");
+                                                (default,'$email','$token','$fechaVencimiento')");
         $consulta->execute();
     }
     
