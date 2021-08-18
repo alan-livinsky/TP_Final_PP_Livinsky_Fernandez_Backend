b772aacc73185cb9820cec4314af81c7c4125f5d
alan-livinsky <alan.livinsky@gmail.com>
2021-08-18 04:47:27 -0300

    as

--- app/index.php
+++ app/index.php
@@ -53,7 +53,7 @@ $app->add(new Tuupola\Middleware\JwtAuthentication([
     "path" => ["/"], 
     "ignore" => ["/Bienvenido","/Usuarios/registro","/Usuarios/loguin",
                 "/Usuarios/recuperarContrase","/cargaDeFuego/listaMateriales",
-                "/cargaDeFuego/datosMaterial","/Usuarios/emailRecuperacion/","/recuperarContrase/modificar"],
+                "/cargaDeFuego/datosMaterial","/Usuarios/emailRecuperacion/","/Usuarios/recuperarContrase/modificar"],
 
     "error" => function ($response, $arguments){
         $data["status"]="error";
@@ -101,7 +101,7 @@ $app->group('/Usuarios', function (RouteCollectorProxy $group){
 
 
     $group->post('/recuperarContrase','enviarEmailDeRecuperacion');
-    $group->post('/recuperarContrase/modificar',\UsuariosController::class.':retornarEstadoRecuperarContraseña');
+    $group->post('/recuperarContraseña/modificar',\UsuariosController::class.':retornarEstadoRecuperarContraseña');
     $group->get('/emailRecuperacion/{selector}/{token}','validarEnlaceRecuperContraseña');
  
 });
