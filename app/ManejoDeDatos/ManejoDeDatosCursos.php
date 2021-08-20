7afac66e03e0455de5da82136d4241425fde956d
alan-livinsky <alan.livinsky@gmail.com>
2021-08-20 05:21:44 -0300

    a

--- app/ManejoDeDatos/ManejoDeDatosCursos.php
+++ app/ManejoDeDatos/ManejoDeDatosCursos.php
@@ -4,18 +4,23 @@
         $listaCursos=buscarlistaDeCursos();
 
         $extraerAños=$listaCursos[0]["col1_values"];
-        $extraerAños=$arr = explode(",", $extraerAños);
+            str_replace("{","",$extraerAños);
+            str_replace("}","",$extraerAños);
+        $extraerAños=$arr = explode(",",$extraerAños);
 
-        var_dump($extraerAños);
-
-
-        $extraerComisiciones=$listaCursos[0]["col2_values"];
-        $extraerComisiciones=$arr = explode(",", $extraerComisiciones);
-        var_dump($extraerComisiciones);
-
+        $extraerComisiones=$listaCursos[0]["col2_values"];
+            str_replace("{","",$extraerAños);
+            str_replace("}","",$extraerAños);
+        $extraerComisiones=$arr = explode(",",$extraerComisiones);
+       
         $ExtraerTurnos=$listaCursos[0]["col3_values"];
+            str_replace("{","",$ExtraerTurnos);
+            str_replace("}","",$ExtraerTurnos);
+        $ExtraerTurnos=$arr = explode(",",$ExtraerTurnos);
 
-        $response->getBody()->write(json_encode($listaCursos));
+        $listaOpcionesDeCurso=[$extraerAños,$extraerComisiones,$ExtraerTurnos];
+
+        $response->getBody()->write(json_encode($listaOpcionesDeCurso));
         return $response;
     }
 
