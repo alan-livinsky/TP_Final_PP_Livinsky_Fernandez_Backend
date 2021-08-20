87d71d5c19eade3360bd21e2b90f5e51bbee0a99
alan-livinsky <alan.livinsky@gmail.com>
2021-08-20 00:39:32 -0300

    sd

--- app/acceso_a_datos/Consultas.php
+++ app/acceso_a_datos/Consultas.php
@@ -16,11 +16,12 @@ function eliminacionSimple($tabla,$campo,$tipoCondicion,$datoAEvaluar){
     //ACA IRIA UN FILTRO POR TIPO DE DATO
 
     $accesoDatos=Acceso_a_datos::obtenerConexionBD(); 
+    echo "DELETE FROM $tabla WHERE vencimiento".$condicion."'$datoAEvaluar'";
+   /*
     $consulta=$accesoDatos->prepararConsulta("DELETE FROM $tabla WHERE vencimiento".$condicion."'$datoAEvaluar'");
 
     echo "DELETE FROM $tabla WHERE vencimiento".$condicion."'$datoAEvaluar'";
-    //echo "DELETE FROM $tabla WHERE $condicion";
-    $consulta->execute();
+    $consulta->execute();*/
 }
 
 ?>
