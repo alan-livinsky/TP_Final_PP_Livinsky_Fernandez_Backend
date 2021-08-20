37a45a2abe187f511854ab75740d81b8cea4f0c2
alan-livinsky <alan.livinsky@gmail.com>
2021-08-19 23:00:51 -0300

    asas

--- app/entidades/Usuarios.php
+++ app/entidades/Usuarios.php
@@ -18,7 +18,7 @@
             $buscarHash->execute();
             $hash=$buscarHash->fetchAll(PDO::FETCH_ASSOC);
         
-            if (password_verify($contraseña,$hash[0]["contraseña"])){
+            if (password_verify($contraseña,$hash["contraseña"])){
                 var_dump($hash);
                 $contraseña=$hash[0]->contraseña;
                 $consulta=$accesoDatos->prepararConsulta("SELECT * FROM usuarios WHERE email='$email' AND contraseña='$contraseña'");
