1d2cc7f4a0ef0171612bd78b026f6afe2a5699b7
alan-livinsky <alan.livinsky@gmail.com>
2021-08-20 06:20:44 -0300

    sa

--- app/Entidades/Cursos.php
+++ app/Entidades/Cursos.php
@@ -4,8 +4,8 @@
     function buscarlistaDeCursos(){
         $accesoDatos=Acceso_a_datos::obtenerConexionBD();
         $consulta=$accesoDatos->prepararConsulta("SELECT 
-                                                    ARRAY(SELECT DISTINCT(año) FROM cursos) AS col1_values,
-                                                    ARRAY(SELECT DISTINCT(comision) FROM cursos) AS col2_values,
+                                                    ARRAY(SELECT DISTINCT(año) FROM cursos ORDER BY año) AS col1_values,
+                                                    ARRAY(SELECT DISTINCT(comision) FROM cursos ORDER BY comision) AS col2_values,
                                                     ARRAY(SELECT DISTINCT(turno) FROM cursos) AS col3_values");
         $consulta->execute();
         return $consulta->fetchAll(PDO::FETCH_ASSOC);
