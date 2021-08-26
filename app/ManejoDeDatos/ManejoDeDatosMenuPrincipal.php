917b451d1f39a2767cddfe8bc08ce8a4cee07657
alan-livinsky <alan.livinsky@gmail.com>
2021-08-26 18:59:21 -0300

    as

--- app/ManejoDeDatos/ManejoDeDatosMenuPrincipal.php
+++ app/ManejoDeDatos/ManejoDeDatosMenuPrincipal.php
@@ -3,8 +3,8 @@
 use Firebase\JWT\JWT;
 
   function retornarAccesoMenuPrincipal($request,$response,$args){
-    return $response->withStatus(200); 
-   // return $response->withHeader('Location','https://tp-final-pp-liv-ferz-frontend.herokuapp.com/Menu_Principal.html')->withStatus(302);
+   // return $response->withStatus(200); 
+  return $response->withHeader('Location','https://tp-final-pp-liv-ferz-frontend.herokuapp.com/Menu_Principal.html')->withStatus(302);
   }
 
   function mantenerAccesoMenuPrincipal($request,$response,$args){
