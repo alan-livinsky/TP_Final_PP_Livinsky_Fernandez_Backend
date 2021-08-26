d4bf3e3941ba711285d89e92570cb18bc51ca878
alan-livinsky <alan.livinsky@gmail.com>
2021-08-26 19:34:20 -0300

    as

--- app/ManejoDeDatos/ManejoDeDatosMenuPrincipal.php
+++ app/ManejoDeDatos/ManejoDeDatosMenuPrincipal.php
@@ -3,12 +3,14 @@
 use Firebase\JWT\JWT;
 
   function retornarAccesoMenuPrincipal($request,$response,$args){
-   // return $response->withStatus(200); 
+  return $response->withStatus(200); 
+
+  /*
   return $response->withHeader('Location','https://tp-final-pp-liv-ferz-frontend.herokuapp.com/Menu_Principal.html')
                   ->withHeader('Access-Control-Allow-Origin', '*')
                   ->withHeader('Access-Control-Allow-Methods', 'get,post,PUT,DELETE,options')
-                  //->withHeader('Location','https://tp-final-pp-liv-ferz-frontend.herokuapp.com/Menu_Principal.html')
                   ->withStatus(302);
+  */
   }
 
   function mantenerAccesoMenuPrincipal($request,$response,$args){
