8405d4aae0009f5790ddb9383703a544aabd537d
alan-livinsky <alan.livinsky@gmail.com>
2021-08-26 19:20:20 -0300

    ASAS

--- app/ManejoDeDatos/ManejoDeDatosMenuPrincipal.php
+++ app/ManejoDeDatos/ManejoDeDatosMenuPrincipal.php
@@ -4,7 +4,9 @@ use Firebase\JWT\JWT;
 
   function retornarAccesoMenuPrincipal($request,$response,$args){
    // return $response->withStatus(200); 
-  return $response->withHeader('Location','https://tp-final-pp-liv-ferz-frontend.herokuapp.com/Menu_Principal.html')->withStatus(305);
+  return $response->withHeader('Location','https://tp-final-pp-liv-ferz-frontend.herokuapp.com/Menu_Principal.html')
+                  //->withHeader('Location','https://tp-final-pp-liv-ferz-frontend.herokuapp.com/Menu_Principal.html')
+                  ->withStatus(302);
   }
 
   function mantenerAccesoMenuPrincipal($request,$response,$args){
