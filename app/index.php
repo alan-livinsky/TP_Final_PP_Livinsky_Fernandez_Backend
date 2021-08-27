15629de89550230f281bc812338452f33f4d1bae
alan-livinsky <alan.livinsky@gmail.com>
2021-08-27 10:59:59 -0300

    asas

--- app/index.php
+++ app/index.php
@@ -110,7 +110,7 @@ $app->group('/Usuarios', function (RouteCollectorProxy $group){
 $app->group('/Cursos', function (RouteCollectorProxy $group){
     $group->get('/Lista','retornarListaCursos');
     $group->get('/ListaFiltrada','retornarListaCursosFiltrada');
-    $group->post('/Asociar','retornarListaCursosFiltrada');
+    $group->post('/Asociar','retornarEstadoActualizacionContraseña');
 });
 
 
