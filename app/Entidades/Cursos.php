4ca9d17595224da3d2efa583c5750d15b534c684
alan-livinsky <alan.livinsky@gmail.com>
2021-08-27 08:54:15 -0300

    asas

--- app/Entidades/Cursos.php
+++ app/Entidades/Cursos.php
@@ -1,5 +1,12 @@
 <?php
 
+    function buscarCurso($año,$comision,$turno){
+        $accesoDatos=Acceso_a_datos::obtenerConexionBD();
+        $consulta=$accesoDatos->prepararConsulta("SELECT id_curso FROM cursos Where año='$año' and comision='$comision' and turno='$turno'");
+        $consulta->execute();
+        return $consulta->fetchAll(PDO::FETCH_ASSOC);
+    }
+
     function buscarlistaDeCursos(){
         $accesoDatos=Acceso_a_datos::obtenerConexionBD();
         $consulta=$accesoDatos->prepararConsulta("SELECT * FROM cursos");
