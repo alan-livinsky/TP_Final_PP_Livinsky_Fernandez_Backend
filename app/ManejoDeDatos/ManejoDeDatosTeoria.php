ce1a0492555506db16b0e4a689d5e807a08e9c50
alan-livinsky <alan.livinsky@gmail.com>
2021-11-09 22:17:42 -0300

    a

--- app/ManejoDeDatos/ManejoDeDatosTeoria.php
+++ app/ManejoDeDatos/ManejoDeDatosTeoria.php
@@ -95,7 +95,7 @@ function retornarListaTeoriaVisualizar($request,$response,$args){
 function retornarNormativaRelacionada($request,$response,$args){
     $ejercicio=$request->getBody();
     $ejercicio=json_decode($ejercicio);
-    $id_ejercicio=$ejercicio->id_usuario;
+    $id_ejercicio=$ejercicio->id_ejercicio;
 
 
     $normativaRelacionada=buscarNormativaRelacionada($id_ejercicio);
