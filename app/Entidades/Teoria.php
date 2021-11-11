e37bdf7a184be38cdc8e00aa9dabcee50d68fb24
alan-livinsky <alan.livinsky@gmail.com>
2021-11-11 19:08:11 -0300

    a

--- app/Entidades/Teoria.php
+++ app/Entidades/Teoria.php
@@ -145,8 +145,9 @@ function buscarListaOpcionesBarraApoyo($id_usuario,$id_ejercicio,$tipo){
                                                     WHERE teoria_cursos.id_ejercicio=$id_ejercicio $filtroProfesores
                                                     UNION
                                                     SELECT teoria_sistema.titulo,teoria_sistema.tipo FROM teoria_sistema
-                                                    WHERE teoria_sistema.id_ejercicio=$id_ejercicio");
+                                                    WHERE teoria_sistema.id_ejercicio=$id_ejercicio");                                    
         $consulta->execute();
+        var_dump($consulta);
         return $consulta->fetchAll(PDO::FETCH_ASSOC);
     }
 
