d2be8a9f498c1d146aa6d362a6a1323ef910ee23
alan-livinsky <alan.livinsky@gmail.com>
2021-11-12 07:40:14 -0300

    A

--- app/Entidades/Teoria.php
+++ app/Entidades/Teoria.php
@@ -126,7 +126,7 @@ function buscarListaOpcionesBarraApoyo($id_usuario,$id_ejercicio,$tipo){
         $filtroProfesores="";
 
         if(count($listaProfesores)==1){
-            $filtroProfesores="AND (teoria_cursos.id_usuario='".$listaProfesores[0]['id_usuario']."'";
+            $filtroProfesores="AND (teoria_cursos.id_usuario='".$listaProfesores[0]['id_usuario']."')";
         }
 
         if(count($listaProfesores)>1){
